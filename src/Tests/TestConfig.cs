097417f5d4678dfb5a4b9e422fb30f1ce89b6312
Simon Cropp <simon.cropp@gmail.com>
2019-11-20 21:38:51 +1100

    Update TestConfig.cs

--- src/Tests/TestConfig.cs
+++ src/Tests/TestConfig.cs
@@ -1,5 +1,3 @@
 ﻿using ApprovalTests.Reporters;
-using Xunit;
 
 [assembly: UseReporter(typeof(DiffReporter), typeof(AllFailingTestsClipboardReporter))]
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
